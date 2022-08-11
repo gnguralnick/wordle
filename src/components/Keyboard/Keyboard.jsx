b821727767ab60dd7c77073ff9c937432340d3de
Gabe Guralnick <gnguralnick@gmail.com>
2022-08-10 21:57:31 -0400

    feat: improve keyboard implementation + styling

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 
+import backspace from '../../icons/backspace.png';
+
 import Styles from './Keyboard.module.scss';
 
 const cx = classNames.bind(Styles);
@@ -11,35 +13,38 @@ const Keyboard = props => {
 
     const rows = [
         'qwertyuiop',
-        'asdfghjkl',
+        ' asdfghjkl ',
         'EzxcvbnmD'
     ]
+
+    const renderChar = (char, index) => {
+        if (char === 'D') {
+            return <button key={index} className={cx('delete')} onClick={() => onClick({key: 'Backspace'})}>
+                <img src={backspace} alt="Delete" />
+            </button>;
+        } else if (char === 'E') {
+            return <button key={index} className={cx('submit')} onClick={() => onSubmit()}>
+                <span>Enter</span>
+            </button>;
+        } else if (char === ' ') {
+            return <div key={index} className={cx('spacer')}></div>;
+        }
+        return <button
+            key={index}
+            onClick={() => onClick({key: char})}
+            className={cx(
+                'char',
+                {'correct': correct.includes(char)},
+                {'kinda': kinda.includes(char)},
+                {'incorrect': incorrect.includes(char)})}>
+            <span>{char}</span>
+        </button>;
+    }
 
     return <div className={cx('keyboard')}>
         {rows.map((row, index) => {
             return <div key={index} className={cx('row')}>
-                {row.split('').map((char, index) => {
-                    if (char === 'D') {
-                        return <button key={index} className={cx('delete')} onClick={() => onClick({key: 'Backspace'})}>
-                            <span>{char}</span>
-                        </button>
-                    } else if (char === 'E') {
-                        return <button key={index} className={cx('submit')} onClick={() => onSubmit()}>
-                            <span>Enter</span>
-                        </button>
-                    }
-                    return <button
-                        key={index}
-                        onClick={() => onClick({key: char})}
-                        className={cx(
-                            'char',
-                            {'correct': correct.includes(char)},
-                            {'kinda': kinda.includes(char)},
-                            {'incorrect': incorrect.includes(char)})}>
-                        <span>{char}</span>
-                    </button>
-                }
-                )}
+                {row.split('').map(renderChar)}
             </div>;})
         }
     </div>
